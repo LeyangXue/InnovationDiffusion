7d604a373c3fdafb170f76671a342fa13f0da37b
leyangx <leyangx@163.com>
2022-10-18 15:45:14 +0800

    sfigure1 v2

--- sfigure1/code/plot.py
+++ sfigure1/code/plot.py
@@ -23,7 +23,50 @@ def PlotAxes(ax,xlabel,ylabel, title, mode=False):
     if mode == True:
         ax.legend(loc='upper left',bbox_to_anchor=(0.00,1.0), framealpha=0, fontsize=30)
             
+def NetworkSort(properities,netsname):
+    
+    statistic = pd.read_csv(result_path+'/complete_datasets.csv')
+    sortofnetwork  = statistic.sort_values(by=properities) #rank the network according to the size of network
+    netsname_rank = [net for net in sortofnetwork['name'] if net in netsname]
+    
+    return netsname_rank
+
+def loadSpreadData(netsortsize):
+    
+    results = pd.read_csv(result_path+'/summary_results.csv')
+    index = list(map(lambda x:round(x,2),np.arange(0,1.01,0.05)))
+    betac_name = pd.DataFrame(index=index,columns=netsortsize)
+    for name in netsortsize:
+        result = results[results['network']==name]
+        betac_name[name] = list(result['betac_mc'] - result['betac_dmp'])
+    
+    return betac_name
+
+def plotviolinplot(betacdata,networklabel,figurename):
+    
+    fontsize = 28
+    font_label = {'family': "Arial", 'size':fontsize}
+    #color = ["#4063a3",'#cfcfcf','#ddc000','#d7aca1',"#34b6c6","#79ad41",'#3b3a3e']
+    
+    fig, ax = plt.subplots(1,1,figsize=(16,8), constrained_layout = True)
+    vp = ax.violinplot(betacdata, np.arange(0,len(netsname),1),widths=0.6,showmeans=False, showmedians=False, showextrema=True)
+    ax.set_ylim(-0.2,0.2)
+    ax.set_xlim(-0.5,37.5)
+    ax.set_xticks(np.arange(0,len(netsname),1))
+    ax.set_xticklabels(networklabel, rotation=90)
+    ax.grid(axis='both')
+    PlotAxes(ax,'Networks',' \n', '')
+    fig.text(0.01,0.35,'Numerical '+ r'$\beta_c$ ' +'- Theoretical '+ r'$\beta_c$',rotation=90, fontdict=font_label)
+    # styling:
+    for body in vp['bodies']:
+        body.set_alpha(0.35)
+        #body.set_facecolor(color[4])
+        body.set_edgecolor('white')   
         
+    plt.savefig(figure_path+'/'+ figurename + '.png', dpi=300)
+    plt.savefig(figure_path+'/'+ figurename + '.eps')
+    plt.savefig(figure_path+'/'+ figurename + '.pdf')
+    
 if __name__ == '__main__':
     
     root_path = 'F:/work/work4_dynamic' #need to change if run this code
@@ -32,75 +75,18 @@ if __name__ == '__main__':
     figure_path = root_path + '/InnovationDiffusion/sfigure1/figure'
     
     #set the label
-    netsname = [each.strip('.txt') for each in sorted(os.listdir(network_path))]
-    labelname  = ['bio-dmela',
-                'bio-yeast-protein-inter',
-                'ca-AstroPh',
-                'ca-AstroPhysTechObs',
-                'ca-BayesianNet',
-                'ca-CSphd',
-                'ca-CondMat',
-                'ca-Data-Mining',
-                'ca-Database-System',
-                'ca-FluidDynamics',
-                'ca-GenTheoFiledParti',
-                'ca-GrQc',
-                'ca-HepPh',
-                'ca-InterdisPhysics',
-                'ca-NetSci2019',
-                'ca-citeseer',
-                'cit-DBLP',
-                'cit-HepPh',
-                'cit-HepTh',
-                'email-EU',
-                'email-EuAll',
-                'email-dnc',
-                'email-enron-large',
-                'soc-Slashdot0811',
-                'soc-academia',
-                'soc-advogato',
-                'soc-delicious',
-                'soc-fb-Huawei',
-                'soc-fb-pages-artist',
-                'soc-fb-pages-company',
-                'soc-fb-pages-government',
-                'soc-fb-pages-media',
-                'soc-musae-git',
-                'tech-as-caida2007',
-                'tech-p2p-gnutella',
-                'tech-pgp',
-                'web-EPA',
-                'web-webbase-2001']
-           
-    #set the parameter
-    fontsize = 28
-    font_label = {'family': "Arial", 'size':fontsize}
-    color = ["#4063a3",'#cfcfcf','#ddc000','#d7aca1',"#34b6c6","#79ad41",'#3b3a3e']
+    netsname = [each.split('.txt')[0] for each in sorted(os.listdir(network_path))]
+    
+    #rank the network according to the size of networks    
+    netsortsize= NetworkSort('N',netsname)
+    #rank the network according to the mean degree of network
+    netsortdegree= NetworkSort('k',netsname)
 
     #load the data
-    results = pd.read_csv(result_path+'/summary_results.csv')
-    index = list(map(lambda x:round(x,2),np.arange(0,1.01,0.05)))
-    betac_name = pd.DataFrame(index=index,columns=netsname)
-    for name in netsname:
-        result = results[results['network']==name]
-        betac_name[name] = list(result['betac_mc'] - result['betac_dmp'])
-    
+    betac_sortsize = loadSpreadData(netsortsize)
+    betac_sortdegree = loadSpreadData(netsortdegree)
+
     #plot the figure
-    fig, ax = plt.subplots(1,1,figsize=(16,8), constrained_layout = True)
-    vp = ax.violinplot(betac_name, np.arange(0,len(netsname),1),widths=0.6,showmeans=False, showmedians=False, showextrema=True)
-    ax.set_ylim(-0.2,0.2)
-    ax.set_xlim(-0.5,37.5)
-    ax.set_xticks(np.arange(0,len(netsname),1))
-    ax.set_xticklabels(labelname, rotation=90)
-    ax.grid(axis='both')
-    PlotAxes(ax,'Networks',' \n', '')
-    fig.text(0.01,0.35,'Numerical '+ r'$\beta_c$ ' +'- Theoretical '+ r'$\beta_c$',rotation=90, fontdict=font_label)
-    # styling:
-    for body in vp['bodies']:
-        body.set_alpha(0.35)
-        #body.set_facecolor(color[4])
-        body.set_edgecolor('white')
-        
-    plt.savefig(figure_path+'/criticalPoint.png', dpi=300)
-    plt.savefig(figure_path+'/criticalPoint.eps')
-    plt.savefig(figure_path+'/criticalPoint.pdf')
+    plotviolinplot(betac_sortsize,netsortsize,'sfigure1_ranksize')
+    plotviolinplot(betac_sortdegree,netsortdegree,'sfigure1_rankdegree')
+    
